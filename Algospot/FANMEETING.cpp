5cfc0f587cfbf505167e75f87bd84aa8c8b8325f
kimbsu00 <58127442+kimbsu00@users.noreply.github.com>
2021-01-06 14:41:50 +0900

    Update FANMEETING.cpp

--- Algospot/FANMEETING.cpp
+++ Algospot/FANMEETING.cpp
@@ -1,88 +1,79 @@
 #include<iostream>
-#include<string>
 #include<vector>
+#include<string>
 
 using namespace std;
 
-bool NOR(vector<char>& member, vector<char>& fan, const int& index);
+int FANMEETING();
+bool NOR(int index);
 
-int main(void)
-{
-	int tc;
-	cin >> tc;
-	vector<int> answer(tc, 0);
+/* 
+	int 형이 아니고  char 형 변수를 사용하는 이유?
+	->	NOR() 함수에서 비트연산자 | 를 사용하는데,
+		int 형 변수보다 char 형 변수에 대해 비트연산을 수행하는 것이 속도가 더 빠르기 때문.
+		실제로 char 를 int 로 변경해서 제출하면 시간초과로 오답처리 됨.
+*/
+vector<char> member;
+vector<char> fan;
 
-	for (int i = 0; i < tc; i++)
-	{
-		string str1, str2;
-		cin >> str1 >> str2;
+int main(void) {
+	int C;
+	cin >> C;
+	vector<int> answer(C);
 
-		vector<char> member(str1.size());		// 멤버들을 저장할 벡터
-		vector<char> fan(str2.size());			// 팬들을 저장할 벡터
-								// 남자는 0, 여자는 1으로 저장 -> NOR 연산을 위함
+	for (int i = 0; i < C; i++) {
+		member.clear();
+		fan.clear();
 
-		for (int n = 0; n < str1.size(); n++)
-		{
-			if (str1.at(n) == 'F')
-				member[n] = 1;
-			else
-				member[n] = 0;
+		string temp1;
+		string temp2;
+		cin >> temp1 >> temp2;
+
+		// 남자는 0 으로, 여자는 1 로 저장한다.
+		for (int k = 0; k < temp1.size(); k++) {
+			if (temp1[k] == 'M')
+				member.push_back(0);
+			else if (temp1[k] == 'F')
+				member.push_back(1);
 		}
-		for (int n = 0; n < str2.size(); n++)
-		{
-			if (str2.at(n) == 'F')
-				fan[n] = 1;
-			else
-				fan[n] = 0;
+		for (int k = 0; k < temp2.size(); k++) {
+			if (temp2[k] == 'M')
+				fan.push_back(0);
+			else if (temp2[k] == 'F')
+				fan.push_back(1);
 		}
 
-		int start = 0;
-		while (true)
-		{
-			// 모든 멤버들이 포옹을 하는 경우
-			if (NOR(member, fan, start++))
-				answer[i]++;
-				
-			if (start + str1.size() > str2.size())
-				break;
-		}
+		answer[i] = FANMEETING();
 	}
 
-	for (int i = 0; i < answer.size(); i++)
+	for (int i = 0; i < C; i++)
 		cout << answer[i] << endl;
-
+	
 	return 0;
 }
 
-bool NOR(vector<char>& member, vector<char>& fan, const int& index)
-{
+int FANMEETING() {
 	int ret = 0;
+	int index = 0;
 
-	for (int i = 0; i < member.size(); i++)
-	{
-		// NOR 연산 수행 -> member 와 fan 모두 남자인 경우, 즉 모두 0 인 경우에만 1 이 더해진다.
-		ret += !(member[i] | fan[i + index]);
+	while (true) {
+		if (NOR(index++))		ret++;
+		if (index + member.size() > fan.size())		break;
 	}
 
-	if (ret == 0)
-		return true;
-	else
-		return false;
+	return ret;
 }
 
-/*
-bool NAND(int& member, vector<char>& fan, const int& size, const int& start)
-{
-	int num = 0;
-	for (int i = 0; i < size; i++)
-		if (fan[i + start])
-			num += pow(2, i);
-	int ret = !(member | num);
-	if (fan[size - 1 + start] == 0)
-		ret = 1;
+bool NOR(int index) {
+	int ret = 0;
+
+	// 모든 멤버들이 포옹을 하는 경우는 멤버와 팬을 NOR 연산 했을 때, 전부 0이 되는 경우이다.
+	for (int i = 0; i < member.size(); i++) {
+		ret += !(member[i] | fan[i + index]);
+	}
+	
 	if (ret == 0)
 		return true;
 	else
 		return false;
 }
-*/
