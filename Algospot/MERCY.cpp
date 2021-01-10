a911478ffac1c896bfe64b50e1dc503ab46231dc
ByeongSu Kim <kimbsu3000@gmail.com>
2021-01-10 20:02:05 +0900

    Create MERCY.cpp

---
+++ Algospot/MERCY.cpp
@@ -0,0 +1,20 @@
+/*
+  url: https://algospot.com/judge/problem/read/MERCY
+*/
+
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main(void) {
+	int C;
+	cin >> C;
+
+	for (int i = 0; i < C; i++) {
+		cout << "Hello Algospot!" << endl;
+	}
+
+	return 0;
+}
