c238b1459d04a7480a13a95f29cab4379d2fb77b
ByeongSu Kim <kimbsu3000@gmail.com>
2021-02-01 17:53:48 +0900

    Update NERD2_STL.cpp

--- Algospot/NERD2_STL.cpp
+++ Algospot/NERD2_STL.cpp
@@ -1,7 +1,8 @@
 /*
   url: https://algospot.com/judge/problem/read/NERD2
   
-  map 을 이용하여 해결함.
+  (p. q) = (x, y) 로 두고 2차원 상의 좌표로 보면,
+  Nerd 인 좌표들은 우하향인 형태로 나타나게 됨을 이용하여 해결함.
 */
 
 #include<iostream>
@@ -44,39 +45,50 @@ int main(void) {
 }
 
 int NERD2(int x, int y) {
+	// (x, y) 가 Nerd 가 아니면 바로 리턴
 	if (!checkNerd(x, y))		return coordinates.size();
 
+	// (x, y) 와 비교했을 때 Nerd 가 아니게 되는 기존 좌표들을 제거
 	removeNotNerd(x, y);
+	// (x, y) 를 추가해주고 리턴
 	coordinates.insert(make_pair(x, y));
 	return coordinates.size();
 }
 
 bool checkNerd(int x, int y) {
-  // x 보다 큰 값들 중에서 가장 아래에 위치한 좌표를 반환
+	// x 좌표가 x 값보다 큰 좌표들 중에서 x 값이 가장 작은 좌표를 it 에 저장
 	map<int, int>::iterator it = coordinates.lower_bound(x);
 
-  // x 보다 큰 값이 없는 경우 (x, y) 는 Nerd 이다.
+	// it 가 end() 인 경우, x 좌표가 x 값보다 큰 좌표가 존재하지 않으므로 y 값과 상관없이 (x, y) 는 nerd 이다.
 	if (it == coordinates.end())		return true;
 
-  // x 보다 큰 값들 중에서 가장 아래에 있는 좌표보다 커야만 너드가 될 수 있다.
+	// it 가 가리키는 좌표는 x보다 오른쪽에 있는 좌표 중 가장 위에 있는 좌표이므로,
+	// 이 점보다 y 좌표가 크면 nerd 이다.
 	return y > it->second;
 }
 
 void removeNotNerd(int x, int y) {
-  // x 보다 큰 값들 중에서 가장 아래에 위치한 좌표를 반환
+	// x 좌표가 x 값보다 큰 좌표들 중에서 x 값이 가장 작은 좌표를 it 에 저장
 	map<int, int>::iterator it = coordinates.lower_bound(x);
 
-  // (x, y) 보다 왼쪽에 있는 좌표가 없는 경우
+	// it 가 begin() 이라면 (x, y) 보다 왼쪽에 위치한 좌표가 존재하지 않음
 	if (it == coordinates.begin())		return;
 
+	// it 가 begin() 이 아니라면 (x, y) 보다 오른쪽에 있는 좌표들 중에서 가장 왼쪽에 있는 좌표를 가리키고 있으므로,
+	// 한 칸 왼쪽으로 이동한다.
 	it--;
+
 	while (true) {
+		// it 의 y 값이 y 보다 크다면 더 이상 비교할 필요가 없다.
+		// -> it 보다 왼쪽에 있는 좌표들은 it 보다 위에 위치해 있기 때문이다.
 		if (it->second > y)		break;
 
+		// it 가 맨 왼쪽에 위치한 좌표라면 해당 좌표를 없애고 반복 종료
 		if (it == coordinates.begin()) {
 			coordinates.erase(it);
 			break;
 		}
+		// 그 외의 경우, 해당 좌표 제거후 왼쪽으로 이동
 		else {
 			map<int, int>::iterator temp = it;
 			temp--;
