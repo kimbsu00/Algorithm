9e175d4f203bae4f771f3fe78afe19f48050129b
ByeongSu Kim <kimbsu3000@gmail.com>
2021-03-05 15:41:29 +0900

    Brute Force, Math
    Memory Usage: 0KB
    Time: 62ms

---
+++ Codeforces/4A_Watermelon.cpp
@@ -0,0 +1,24 @@
+/*
+  url: https://codeforces.com/problemset/problem/4/A
+*/
+
+#include<iostream>
+ 
+using namespace std;
+ 
+int main(void) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int w;
+    cin>>w;
+    
+    if(w>2 && !(w%2)) {
+        cout<<"YES";
+    }
+    else {
+        cout<<"NO";
+    }
+    
+    return 0;
+}
