a24cfc2b3a9767a7a9ff89fa5fc557ec79eb5b37
ByeongSu Kim <kimbsu3000@gmail.com>
2023-01-29 14:04:46 +0900

    Tree

--- Baekjoon/1967_트리의지름.cpp
+++ Baekjoon/1967_트리의지름.cpp
@@ -1,66 +1,85 @@
 /*
-    url: https://www.acmicpc.net/problem/1967
+url: https://www.acmicpc.net/problem/1967
     
-    "리프 - 루트 - 리프" 가 최장거리인 경우 1가지
-    "리프 - 루트가 아닌 다른 노드 - 리프" 가 최장거리인 경우 1가지
-    이렇게 총 2 가지의 경우를 고려해서 해결함.
-    
-    tree 변수에는 부모 노드에서 자식 노드로 가는 엣지만 추가했는데, 이는 재귀호출 과정에서 자식 노드에서 부모 노드로 다시 올라가는 일을 차단하기 위함.
-    사실 자식 노드에서 부모 노드로 가는 엣지도 추가하고, vector<bool> visited 변수 하나 추가해서 이미 방문했는지 여부를 따로 저장해도 되지만,
-    메모리 절약을 위해서 트리를 방향 그래프처럼 저장해서 해결함
+"리프 - 루트 - 리프" 가 최장거리인 경우 1가지
+"리프 - 루트가 아닌 다른 노드 - 리프" 가 최장거리인 경우 1가지
+이렇게 총 2 가지의 경우를 고려해서 해결함.
 */
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
- 
-int solve(int node, int sum);
 
-// tree[parent] = pair<child, weight>
-vector<vector<pair<int, int>>> tree;
+class Edge
+{
+public:
+    int child;
+    int weight;
+
+    Edge() : child(-1), weight(-1) {}
+
+    Edge(int p_child, int p_weight) : child(p_child), weight(p_weight) {}
+};
+
+int N;
+vector<vector<Edge>> tree;
 vector<int> diameter;
 
-int main(void) {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+int solve(int node, int sum)
+{
+    if (tree[node].empty())
+    {
+        return sum;
+    }
 
-	int N;
-	cin >> N;
-	tree = vector<vector<pair<int, int>>>(N, vector<pair<int, int>>());
+    vector<int> distance;
 
-	N--;
-	while (N--) {
-		int parent, child, weight;
-		cin >> parent >> child >> weight;
+    for (int idx = 0; idx < tree[node].size(); idx++)
+    {
+        int child = tree[node][idx].child;
+        int weight = tree[node][idx].weight;
 
-		tree[parent - 1].push_back(make_pair(child - 1, weight));
-	}
-	
-	int max = solve(0, 0);
-	for (int d : diameter) {
-		d > max ? max = d : d;
-	}
+        int dist = solve(child, sum + weight);
+        distance.push_back(dist);
+    }
 
-	cout << max << "\n";
+    if (distance.size() > 1)
+    {
+        sort(distance.begin(), distance.end(), greater<int>());
+        diameter.push_back(distance[0] + distance[1] - 2 * sum);
+    }
 
-	return 0;
+    return distance[0];
 }
 
-int solve(int node, int sum) {
-	if (tree[node].empty())		return sum;
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
-	vector<int> weight;
+    cin >> N;
+    tree = vector<vector<Edge>>(N, vector<Edge>());
 
-	for (int i = 0; i < tree[node].size(); i++) {
-		weight.push_back(solve(tree[node][i].first, sum + tree[node][i].second));
-	}
+    int parent, child, weight;
+    for (int idx = 0; idx < N - 1; idx++)
+    {
+        cin >> parent >> child >> weight;
 
-	if (weight.size() > 1) {
-		sort(weight.begin(), weight.end(), greater<int>());
-		diameter.push_back(weight[0] + weight[1] - 2 * sum);
-	}
+        tree[parent - 1].push_back(Edge(child - 1, weight));
+    }
 
-	return weight[0];
+    int answer = solve(0, 0);
+    for (int d : diameter)
+    {
+        if (answer < d)
+        {
+            answer = d;
+        }
+    }
+
+    cout << answer << "\n";
+
+    return 0;
 }
