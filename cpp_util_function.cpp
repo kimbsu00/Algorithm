4753a03b9015cfb20df9549182bf6f218fbad96f
Byeongsu Kim <kimbsu3000@gmail.com>
2023-08-31 21:32:13 +0900

    CPP UTIL FUNCTION

---
+++ cpp_util_function.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+vector<string> split(string input, char delimiter) {
+    vector<string> answer;
+    stringstream ss(input);
+    string temp;
+
+    while (getline(ss, temp, delimiter)) {
+        answer.push_back(temp);
+    }
+
+    return answer;
+}
