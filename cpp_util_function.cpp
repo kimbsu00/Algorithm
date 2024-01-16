d06f1d1e161ca2336b9adec2e8d27392303e9c1a
Byeongsu Kim <kimbsu3000@gmail.com>
2024-01-16 22:34:33 +0900

    add: Custom Struct priority_queue

--- cpp_util_function.cpp
+++ cpp_util_function.cpp
@@ -15,3 +15,24 @@ vector<string> split(string input, char delimiter) {
 
     return answer;
 }
+
+// priority_queue with Custom Struct
+struct Tree
+{
+    int row, col, age;
+};
+
+struct CompareTree
+{
+    /**
+     * Priority Queue에 대하여, Tree 구조체의 age를 기준으로 오름차순 정렬하기 위해 사용함
+     * PQ의 디폴트는 내림차순 정렬임
+     */
+    bool operator()(Tree &left, Tree &right)
+    {
+        return left.age > right.age;
+    }
+};
+
+// priority_queue 선언 방법
+priority_queue<Tree, vector<Tree>, CompareTree> pq;
